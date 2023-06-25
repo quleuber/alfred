01e54510ce4dea0a95db033d7e1da983c4568f75
Kelvin Steiner <me@steinerkelvin.dev>
2023-06-25 13:27:40 -0300

    init prisma

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Alfred";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+  outputs = { self, nixpkgs }:
+
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        openssl
+        # prisma-engines
+        nodePackages.prisma
+      ];
+    };
+  };
+}
