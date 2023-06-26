3f84b6194164e6260fa76c28d82cfe4251f57ba0
Kelvin Steiner <me@steinerkelvin.dev>
2023-06-26 09:01:50 -0300

    fix prisma nix integration
    thanks to https://github.com/prisma/prisma/issues/3026#issuecomment-927258138

--- flake.nix
+++ flake.nix
@@ -2,20 +2,27 @@
   description = "Alfred";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShell.${system} = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        # openssl
-        # prisma-engines
-        nodePackages.prisma
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+            prisma-engines
+            nodePackages.prisma
+          ];
+          shellHook = let prisma-engines = pkgs.prisma-engines; in ''
+            export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          '';
+        };
+      });
 }
