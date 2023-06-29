67ca9edad0f542ac37f77c382e338b2bd760537b
Kelvin Steiner <me@steinerkelvin.dev>
2023-06-29 10:56:27 -0300

    add `bastion` and `tide` http framework

--- crates/alfred-core/src/main.rs
+++ crates/alfred-core/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use bastion::prelude::*;
+use serde::Deserialize;
+use tide::Request;
+// use tide::prelude::*;
+
+#[fort::root]
+
+async fn main(_: BastionContext) -> Result<(), ()> {
+    let mut app = tide::new();
+    app.at("/").get(|_| async move { Ok("Hello, world!") });
+    app.at("/orders/shoes").post(order_shoes);
+    let res = app.listen("127.0.0.1:8080").await;
+    match res {
+        Err(e) => panic!("Error: {}", e),
+        Ok(_) => Ok(())
+    }
+}
+
+async fn order_shoes(mut req: Request<()>) -> tide::Result {
+    let Animal { name, legs } = req.body_json().await?;
+    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+}
+
+#[derive(Debug, Deserialize)]
+struct Animal {
+    name: String,
+    legs: u16,
 }
