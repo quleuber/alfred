a8d5cd5d99806b118103c3156f0fc48398a90bc9
Kelvin Steiner <me@steinerkelvin.dev>
2023-07-02 15:52:00 -0300

    WIP proxy

--- arion-compose.nix
+++ arion-compose.nix
@@ -1,7 +1,23 @@
 { pkgs, ... }:
 {
-  project.name = "webapp";
+  project.name = "alfred";
   services = {
+
+    traefik = {
+      service = {
+        image = "traefik:v2.10";
+        command = [ "--api.insecure=true" "--providers.docker" ];
+        ports = [
+          "80:80"
+          "443:443"
+          "8080:8080"
+        ];
+        volumes = [
+          "/var/run/docker.sock:/var/run/docker.sock"
+          "./data/traefik/:/etc/traefik/"
+        ];
+      };
+    };
 
     webserver = {
       image.enableRecommendedContents = true;
